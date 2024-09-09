6116f57d2cc9a8993b94531da6975325efd34042
subhadip <subhadippati30@gmail.com>
2024-09-09 10:51:26 +0530

    cmd

---
+++ src/Arrays/Sort0s1sand2s.java
@@ -0,0 +1,12 @@
+package Arrays;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Sort0s1sand2s {
+
+    public void sort012(ArrayList<Integer> arr) {
+        // code here
+        Collections.sort(arr);
+    }
+}
