7cb556d3dfeada613fed2872d6205f7fc126b4eb
subhadip <subhadippati30@gmail.com>
2024-10-09 19:35:40 +0530

    cmd

--- src/Arrays/CheckIfNAndItsDoubleExist.java
+++ src/Arrays/CheckIfNAndItsDoubleExist.java
@@ -1,2 +1,19 @@
-package Arrays;public class CheckIfNAndItsDoubleExist {
+package Arrays;
+
+public class CheckIfNAndItsDoubleExist {
+
+    class Solution {
+        public boolean checkIfExist(int[] arr) {
+
+            for(int i=0; i<arr.length; i++){
+                for(int j=0; j<arr.length; j++){
+                    if(i != j && arr[i] == 2 * arr[j]){
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+
 }
