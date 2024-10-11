40c7183fb570eb98a165d8e6be0a0ffa6196ae34
subhadip <subhadippati30@gmail.com>
2024-10-11 22:29:38 +0530

    cmd

--- src/Arrays/KthMissingPositiveNumber.java
+++ src/Arrays/KthMissingPositiveNumber.java
@@ -12,6 +12,29 @@ public class KthMissingPositiveNumber {
             return k;
         }
     }
+// Using Binary Search approach
 
 
+        public int findKthPositive(int[] arr, int k) {
+            return binarySearch(arr, k);
+
+        }
+        private int binarySearch(int[] arr, int k){
+            int low = 0;
+            int high = arr.length - 1;
+
+            while(low <= high){
+                int mid = low + (high - low) / 2;
+                int missing = arr[mid] - (mid  + 1);
+
+                if(missing < k){
+                    low = mid + 1;
+                }else{
+                    high = mid - 1;
+                }
+
+            }
+
+            return k + high + 1;
+        }
 }
