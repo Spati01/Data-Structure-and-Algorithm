559544b77bddc1d7e3d946608ae85e887743836c
subhadip <subhadippati30@gmail.com>
2024-10-11 22:18:19 +0530

    cmd

---
+++ src/Arrays/KthMissingPositiveNumber.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+public class KthMissingPositiveNumber {
+    class Solution {
+        public int findKthPositive(int[] arr, int k) {
+
+            for(int i : arr){
+                if(i <= k){
+                    k++;
+                }
+            }
+            return k;
+        }
+    }
+
+
+}
