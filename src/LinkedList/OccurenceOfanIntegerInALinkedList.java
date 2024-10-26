a4da3986d965f7c7e8c96b4b69b44861253adb6a
subhadip <subhadippati30@gmail.com>
2024-10-26 12:40:30 +0530

    cmd

---
+++ src/LinkedList/OccurenceOfanIntegerInALinkedList.java
@@ -0,0 +1,33 @@
+package LinkedList;
+
+public class OccurenceOfanIntegerInALinkedList {
+
+
+    class Node
+    {
+        int data;
+        Node next;
+        Node(int key)
+        {
+            data = key;
+            next = null;
+        }
+    }
+    // complete the below function
+    class Solution {
+        public static int count(Node head, int key) {
+            // code here
+            Node temp = head;
+            int count = 0;
+            while(temp != null){
+                if(temp.data == key){
+                    count++;
+                }
+                temp = temp.next;
+            }
+
+            return count;
+
+        }
+    }
+}
