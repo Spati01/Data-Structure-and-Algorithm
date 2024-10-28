5c59ab858b33a72dea69b1a32a402c4bc385c645
subhadip <subhadippati30@gmail.com>
2024-10-28 11:16:44 +0530

    cmd

---
+++ src/Arrays/LongestSquareStreakInAnArray.java
@@ -0,0 +1,52 @@
+package Arrays;
+
+import java.util.*;
+
+
+public class LongestSquareStreakInAnArray {
+    class Solution {
+        public int longestSquareStreak(int[] nums) {
+            Arrays.sort(nums);
+            int ans = -1;
+
+            // HashSet<Integer> set = new HashSet<>();
+
+            for(int i=0; i<nums.length; i++){
+                long x = (long) nums[i];
+                long val = x * x;
+
+                if(val <= Integer.MAX_VALUE && binarySearch(nums,(int)val)){
+                    int len = 1;
+
+
+                    while(val <= Integer.MAX_VALUE && binarySearch(nums,(int)val)){
+                        len++;
+                        val *= val;
+                    }
+                    ans = Math.max(ans, len);
+                }
+            }
+            return ans < 2 ? -1 : ans;
+
+
+        }
+
+        private boolean binarySearch(int[] arr, int key){
+            int l = 0;
+            int h = arr.length - 1;
+            while(l <= h){
+                int mid = l + (h - l) / 2;
+                if(arr[mid] == key){
+                    return true;
+                }
+                if(arr[mid] > key){
+                    h = mid - 1;
+                }else{
+                    l = mid + 1;
+                }
+
+            }
+            return false;
+        }
+    }
+}
