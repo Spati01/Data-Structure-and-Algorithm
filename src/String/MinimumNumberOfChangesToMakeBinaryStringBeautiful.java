5b5a7665beebccdb3b29eb2e3dfdadaeffff954e
subhadip <subhadippati30@gmail.com>
2024-11-05 09:19:02 +0530

    cmd

---
+++ src/String/MinimumNumberOfChangesToMakeBinaryStringBeautiful.java
@@ -0,0 +1,17 @@
+package String;
+
+public class MinimumNumberOfChangesToMakeBinaryStringBeautiful {
+
+
+    class Solution {
+        public int minChanges(String s) {
+            int count = 0;
+            for(int i=1; i<s.length(); i+=2){
+                if(s.charAt(i-1) != s.charAt(i)){
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
