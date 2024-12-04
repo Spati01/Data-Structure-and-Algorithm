c9ee9fe74a083bf3dcc4192c467f5553040476d7
subhadip <subhadippati30@gmail.com>
2024-12-04 09:41:02 +0530

    cmd

---
+++ src/String/StringsRotationsOfEachOther.java
@@ -0,0 +1,17 @@
+package String;
+
+public class StringsRotationsOfEachOther {
+
+
+    class Solution {
+        // Function to check if two strings are rotations of each other or not.
+        public static boolean areRotations(String s1, String s2) {
+
+            if(s1.equals(s2)){
+                return true;
+            }
+            return (s1 + s1).lastIndexOf(s2) > 0;
+        }
+    }
+
+}
