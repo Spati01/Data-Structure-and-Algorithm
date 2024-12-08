e0184c960fdf0a478c841f46257891af78ed1a48
subhadip <subhadippati30@gmail.com>
2024-12-08 23:19:02 +0530

    cmd

---
+++ src/Queue/OverlappingIntervals.java
@@ -0,0 +1,38 @@
+package Queue;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class OverlappingIntervals {
+
+
+    class Solution {
+        public List<int[]> mergeOverlap(int[][] arr) {
+            // Code here // Code here
+
+
+
+            PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+            for(int[] interval : arr){
+                pq.offer(interval);
+            }
+
+            List<int[]> ans  = new ArrayList<>();
+            int[] current = pq.poll();
+
+            while(!pq.isEmpty()){
+                int[] next = pq.poll();
+
+                if(current[1] >= next[0]){
+                    current[1] = Math.max(current[1] , next[1]);
+                }else{
+                    ans.add(current);
+                    current = next;
+                }
+            }
+            ans.add(current);
+            return ans;
+        }
+    }
+}
