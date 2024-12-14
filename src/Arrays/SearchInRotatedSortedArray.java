24dbee5314bf475cb40cb845dd73568b7c205970
subhadip <subhadippati30@gmail.com>
2024-12-14 20:56:35 +0530

    cmd

---
+++ src/Arrays/SearchInRotatedSortedArray.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+public class SearchInRotatedSortedArray {
+
+
+    class Solution {
+        int search(int[] arr, int key) {
+            // Complete this function
+
+            for(int i=0; i<arr.length; i++){
+                if(arr[i] == key){
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
