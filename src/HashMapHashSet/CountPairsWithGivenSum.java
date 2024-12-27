5ed6334f8d2831795fff17f6da402248ca52a6a5
subhadip <subhadippati30@gmail.com>
2024-12-27 08:48:12 +0530

    cmd

---
+++ src/HashMapHashSet/CountPairsWithGivenSum.java
@@ -0,0 +1,26 @@
+package HashMapHashSet;
+
+import java.util.HashMap;
+
+public class CountPairsWithGivenSum {
+
+    class Solution {
+
+        int countPairs(int arr[], int target) {
+            // Your code here
+
+            HashMap<Integer, Integer> map = new HashMap<>();
+            int count = 0;
+            for(int num : arr){
+                if(map.containsKey(target - num)){
+
+                    count += map.get(target - num);
+                }
+
+                map.put(num, map.getOrDefault(num, 0)+ 1);
+            }
+            return count;
+        }
+    }
+
+}
