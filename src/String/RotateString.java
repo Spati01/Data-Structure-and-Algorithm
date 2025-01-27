bff67f507b7fc9354eddcfe1baaac6659ec650ad
subhadip <subhadippati30@gmail.com>
2025-01-27 11:55:19 +0530

    cmd

---
+++ src/String/RotateString.java
@@ -0,0 +1,26 @@
+package String;
+
+public class RotateString {
+    class Solution {
+        public boolean rotateString(String s, String goal) {
+
+            for(int i=0; i<s.length(); i++){
+                String  rotate = isRotate(s, i);
+                if(rotate.equals(goal)){
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private String isRotate(String s, int k){
+
+            k %= s.length();
+
+            String part1 = s.substring(k);
+            String part2 = s.substring(0, k);
+
+            return part1 + part2;
+        }
+    }
+}
