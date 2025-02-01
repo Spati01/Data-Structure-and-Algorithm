269d441540b8df70066a755b20477a993415d7b9
subhadip <subhadippati30@gmail.com>
2025-02-01 10:33:20 +0530

    cmd

---
+++ src/Arrays/SpecialArrayI.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+public class SpecialArrayI {
+
+    class Solution {
+        public boolean isArraySpecial(int[] nums) {
+
+            if(nums.length == 1)return true;
+
+            for(int i=0; i<nums.length-1; i++){
+                if((nums[i] & 1) == (nums[i+1] & 1)){
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
