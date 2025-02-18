fc52df922bf9d023c8f382c01373015a44ee8d73
subhadip <subhadippati30@gmail.com>
2025-02-18 20:00:16 +0530

    cmd

---
+++ src/Arrays/KClosestPointsToOrigin.java
@@ -0,0 +1,27 @@
+package Arrays;
+
+import java.util.*;
+
+public class KClosestPointsToOrigin {
+
+
+    class Solution {
+        public int[][] kClosest(int[][] points, int k) {
+            // Your code here
+            int[][] res = new int[k][2];
+
+            Arrays.sort(points, (a,b)-> {
+                return (a[0] * a[0] + a[1] * a[1])
+                        - (b[0] * b[0] + b[1] * b[1]);
+            });
+
+            int idx = 0;
+            while(idx < k){
+                res[idx] = points[idx];
+                idx++;
+            }
+            return res;
+        }
+    }
+
+}
