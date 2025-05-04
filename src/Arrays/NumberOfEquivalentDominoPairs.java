6bcf973ac945cf0acbb2dda3b982f77328561a2f
subhadip <subhadippati30@gmail.com>
2025-05-04 18:29:33 +0530

    new V

--- src/Arrays/NumberOfEquivalentDominoPairs.java
+++ src/Arrays/NumberOfEquivalentDominoPairs.java
@@ -24,4 +24,43 @@ public class NumberOfEquivalentDominoPairs {
             return countPair;
         }
     }
+    class Helper {
+        public int numEquivDominoPairs(int[][] dominoes) {
+
+            int count = 0;
+            int[] freq = new int[100];
+
+            for (int[] d : dominoes) {
+                int a = Math.min(d[0], d[1]);
+                int b = Math.max(d[0], d[1]);
+                int key = a * 10 + b;
+
+                count+=freq[key]++;
+            }
+
+
+            return count;
+        }
+    }
+
+
+    class Solver {
+        public int numEquivDominoPairs(int[][] dominoes) {
+
+            int count = 0;
+            HashMap<String, Integer> map = new HashMap<>();
+
+            for (int[] d : dominoes) {
+                int a = Math.min(d[0], d[1]);
+                int b = Math.max(d[0], d[1]);
+                String key = a + "," + b;
+
+                count += map.getOrDefault(key, 0);
+                map.put(key, map.getOrDefault(key, 0) + 1);
+            }
+
+
+            return count;
+        }
+    }
 }
